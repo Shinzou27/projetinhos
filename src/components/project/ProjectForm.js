930e05c067670d0b22c693b4d7e3cb1672a08cff
felipe <felipecassianobarbosa@hotmail.com>
2023-10-29 21:50:37 -0300

    feat: add conditional rendering

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,74 +1,83 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 import Input from '../form/Input'
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 import styles from './ProjectForm.module.css'
+import Category from '../types/Category';
 
-function ProjectForm({handleSubmit, btnText, projectData}) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
 
     const [categories, setCategories] = useState([])
     const [project, setProject] = useState(projectData || {})
+    const [progress, setProgress] = useState(false);
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setCategories(data)
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-        .catch(err => console.log(err))
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch(err => console.log(err))
     }, [])
-    
+
     const submit = (e) => {
         e.preventDefault()
-        handleSubmit(project)
+        console.log(project)
+        if (progress) {
+            handleSubmit(project)
+        } else {
+            setProgress(true);
+        }
     }
 
     function handleChange(e) {
-        setProject({...project, [e.target.name]: e.target.value})
+        setProject({ ...project, [e.target.name]: e.target.value })
     }
     function handleSelect(e) {
-        setProject({...project, category: {
-            id: e.target.value,
-            name: e.target.options[e.target.selectedIndex].text
+        setProject({
+            ...project, category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text
             },
         })
-        console.log(project)
     }
 
 
-    return ( 
+    return (
         <form onSubmit={submit} className={styles.form}>
-            <Input
-                type="text"
-                text="Nome do Projeto"
-                name="name"
-                placeholder="Insira o nome do projeto"
-                handleOnChange={handleChange}
-            />
-            <Input
-                type="number"
-                text="Mora atual"
-                name="budget"
-                placeholder="Insira a quantidade de Mora atual"
-                handleOnChange={handleChange}
-            />
-            <Select
-                name = "category_id"
-                text="Selecione a categoria"
-                options={categories}
-                handleOnChange={handleSelect}
-                value={project.category ? project.category.id : ''}
-            />
-            <SubmitButton text={btnText}/>
-        </form>
-        );
+            {!progress ?
+                <>
+                    <Input
+                        type="text"
+                        text="Nome do Projeto"
+                        name="name"
+                        placeholder="Insira o nome do projeto"
+                        handleOnChange={handleChange}
+                    />
+                    <Select
+                        name="category_id"
+                        text="Selecione a categoria"
+                        options={categories}
+                        handleOnChange={handleSelect}
+                        value={project.category ? project.category.id : ''}
+                    />
+                    <SubmitButton text={btnText} />
+                </>
+                :
+                <>
+                    <Category id={project.category.id - 1} projectData={project} setProjectData={setProject} />
+                    <SubmitButton text={"Finalizar"} />
+                </>
+            }
+        </form >
+    );
 }
 
 export default ProjectForm;
