a2c34f18ea68e9f2df0a0642c462b1748c18b38e
felipe <felipecassianobarbosa@hotmail.com>
2023-10-30 01:09:51 -0300

    feat: add contact page

--- src/components/pages/Contato.js
+++ src/components/pages/Contato.js
@@ -1,5 +1,17 @@
+import { BsGithub } from "react-icons/bs";
+import styles from './Contato.module.css'
 function Contato() {
-    return ( <h1>Contato</h1> );
+    return (
+        <div className={styles.about_us}>
+            <h1>Encontre mais <span>projetinhos</span> como este em:</h1>
+            <div>
+                <a target="_blank" href="https://github.com/Shinzou27">
+                    <BsGithub />/shinzou27
+                </a>
+            <hr />
+            </div>
+        </div>
+    );
 }
 
 export default Contato;
