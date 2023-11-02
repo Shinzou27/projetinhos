eb7a5672f1e8ca47b1635e6e020b4500e202df14
felipe <felipecassianobarbosa@hotmail.com>
2023-11-02 18:25:42 -0300

    feat: better skill inputs

--- src/components/types/CategoryForm.js
+++ src/components/types/CategoryForm.js
@@ -1,7 +1,11 @@
 import Input from '../form/Input'
+import styles from './CategoryForm.module.css'
 
 function CategoryForm({ projectData, setProjectData }) {
     function handleChange(e) {
+        if(e.target.type == 'checkbox') {
+            setProjectData({ ...projectData, [e.target.name]: e.target.checked })
+        }
         setProjectData({ ...projectData, [e.target.name]: e.target.value })
         console.log(projectData);
     }
@@ -13,36 +17,69 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="item_name"
                 placeholder="Nome do personagem"
                 handleOnChange={handleChange}
+                value={projectData.item_name ? projectData.item_name : ''}
             />
-            <p>Separe o nível atual da skill e o nível desejado por vírgula</p>
-            <p>Ex.: 1, 10</p>
-            <Input
-                type="text"
-                text="Nível do Auto-Attack"
-                name="aa"
-                placeholder=""
-                handleOnChange={handleChange}
-            />
-            <Input
-                type="text"
-                text="Nível da Habilidade Elemental"
-                name="skill"
-                placeholder=""
-                handleOnChange={handleChange}
-            />
-            <Input
-                type="text"
-                text="Nível da Explosão Elemental"
-                name="ult"
-                placeholder=""
-                handleOnChange={handleChange}
-            />
+            <div className={styles.level}>
+                <Input
+                    type="text"
+                    text="Auto-Attack atual"
+                    name="aa_1"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.aa_1 ? projectData.aa_1 : ''}
+                />
+                <Input
+                    type="text"
+                    text="Auto-Attack desejado"
+                    name="aa_2"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.aa_2 ? projectData.aa_2 : ''}
+                />
+            </div>
+            <div className={styles.level}>
+                <Input
+                    type="text"
+                    text="Habilidade Elemental atual"
+                    name="skill_1"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.skill_1 ? projectData.skill_1 : ''}
+                />
+                <Input
+                    type="text"
+                    text="Habilidade Elemental desejado"
+                    name="skill_2"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.skill_2 ? projectData.skill_2 : ''}
+                />
+            </div>
+            <div className={styles.level}>
+                <Input
+                    type="text"
+                    text="Explosão Elemental atual"
+                    name="ult_1"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.ult_1 ? projectData.ult_1 : ''}
+                />
+                <Input
+                    type="text"
+                    text="Explosão Elemental desejado"
+                    name="ult_2"
+                    placeholder=""
+                    handleOnChange={handleChange}
+                    value={projectData.ult_2 ? projectData.ult_2 : ''}
+                />
+            </div>
             <Input
                 type="number"
                 text="Quantidade de livros T4 atual"
                 name="books"
                 placeholder="Lembre-se de também contabilizar os livros azuis."
                 handleOnChange={handleChange}
+                value={projectData.books ? projectData.books : ''}
             />
         </div>
 
@@ -54,6 +91,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="item_name"
                 placeholder="Nome do personagem"
                 handleOnChange={handleChange}
+                value={projectData.item_name ? projectData.item_name : ''}
             />
             <Input
                 type="date"
@@ -61,6 +99,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="end_date"
                 placeholder="Insira a data de fim do banner desejado"
                 handleOnChange={handleChange}
+                value={projectData.end_date ? projectData.end_date : ''}
             />
             <Input
                 type="number"
@@ -68,6 +107,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="current_primos"
                 placeholder="Insira sua quantidade atual de Primogems"
                 handleOnChange={handleChange}
+                value={projectData.current_primos ? projectData.current_primos : ''}
             />
             <Input
                 type="checkbox"
@@ -75,6 +115,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="welkin"
                 placeholder=""
                 handleOnChange={handleChange}
+                value={projectData.welkin ? projectData.welkin : ''}
             />
         </div>;
 
@@ -82,10 +123,11 @@ function CategoryForm({ projectData, setProjectData }) {
         <div>
             <Input
                 type="text"
-                text="Insira o nome da arma"
+                text="Insira o nome do personagem"
                 name="item_name"
-                placeholder="Nome da arma"
+                placeholder="Nome do personagem"
                 handleOnChange={handleChange}
+                value={projectData.item_name ? projectData.item_name : ''}
             />
             <Input
                 type="number"
@@ -93,6 +135,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="weaponMaterial_t5"
                 placeholder=""
                 handleOnChange={handleChange}
+                value={projectData.weaponMaterial_t5 ? projectData.weaponMaterial_t5 : ''}
             />
             <Input
                 type="number"
@@ -100,6 +143,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="weaponMaterial_t4"
                 placeholder=""
                 handleOnChange={handleChange}
+                value={projectData.weaponMaterial_t4 ? projectData.weaponMaterial_t4 : ''}
             />
             <Input
                 type="number"
@@ -107,6 +151,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="weaponMaterial_t3"
                 placeholder=""
                 handleOnChange={handleChange}
+                value={projectData.weaponMaterial_t3 ? projectData.weaponMaterial_t3 : ''}
             />
             <Input
                 type="number"
@@ -114,6 +159,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="weaponMaterial_t2"
                 placeholder=""
                 handleOnChange={handleChange}
+                value={projectData.weaponMaterial_t2 ? projectData.weaponMaterial_t2 : ''}
             />
         </div>
 
@@ -125,6 +171,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="item_name"
                 placeholder="Nome do personagem"
                 handleOnChange={handleChange}
+                value={projectData.item_name ? projectData.item_name : ''}
             />
             <Input
                 type="number"
@@ -132,6 +179,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="char_level_current"
                 placeholder="Nível do personagem"
                 handleOnChange={handleChange}
+                value={projectData.char_level_current ? projectData.char_level_current : ''}
             />
             <Input
                 type="number"
@@ -139,6 +187,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="char_level_future"
                 placeholder="Nível do personagem"
                 handleOnChange={handleChange}
+                value={projectData.char_level_future ? projectData.char_level_future : ''}
             />
             <Input
                 type="number"
@@ -146,6 +195,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="char_asc_current"
                 placeholder="Ascensão atual"
                 handleOnChange={handleChange}
+                value={projectData.char_asc_current ? projectData.char_asc_current : ''}
             />
             <Input
                 type="number"
@@ -153,6 +203,7 @@ function CategoryForm({ projectData, setProjectData }) {
                 name="char_asc_future"
                 placeholder="Ascensão desejada"
                 handleOnChange={handleChange}
+                value={projectData.char_asc_future ? projectData.char_asc_future : ''}
             />
         </div>
     const categories = [Talent, Roll, Weapon, Ascension];
